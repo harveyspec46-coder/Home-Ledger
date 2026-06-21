794ab6e9163f340372f33872d8d52917a9807ece
harveyspec46-coder <harveyspec46@gmail.com>
2026-06-21 10:03:07 -0700

    Delete Settings.js

--- Settings.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SettingsSchema = new mongoose.Schema({
-  key: { type: String, default: "household", unique: true },
-  title: { type: String, default: "Home Staff Ledger" },
-});
-
-module.exports = mongoose.models.Settings || mongoose.model("Settings", SettingsSchema);
